392acd9aef4c9ffe609dc9c8da8919c5123be3fc
debysouza <debsdebbie90@gmail.com>
2024-09-09 21:21:52 -0300

    Logger - Aula 09/09/24

--- exemplo/src/br/com/exemplo/principal/Sistema.java
+++ exemplo/src/br/com/exemplo/principal/Sistema.java
@@ -1,16 +1,30 @@
 package br.com.exemplo.principal;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import br.com.exemplo.conteudos.Disciplina;
+import br.com.exemplo.utils.Util;
 
 public class Sistema {
 
+	//chamada do Logger
+	private static Logger log = Util.setupLogger();
+	
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Sistema.class.getName());
 		Disciplina disciplina;
 		disciplina = new Disciplina();
 		disciplina.setNome("POO");
 		disciplina.setProfessor("Déby");
 		disciplina.setNivel(3);
+		
 		System.out.println(disciplina);
+		logger.info(disciplina::toString); //com informações adicionais do Logger
+		String dados = disciplina.toString() + "Teste";
+		log.log(Level.INFO, dados); //Logger formatado
+		log.log(Level.INFO, disciplina::toString);
+		log.log(Level.INFO, "Teste realizado!");
 		
 		Disciplina disciplinaParametrizada = new Disciplina("POO", "Déby", 3);
 		System.out.println(disciplinaParametrizada);
